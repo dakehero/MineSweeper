5967d88f7762d9c60facbe767bc64648105ee9a7
dake_ <dake_hero@hotmail.com>
2017-05-30 17:47:08 +0800

    almost finished

--- src/MineSweeperActionListener.java
+++ src/MineSweeperActionListener.java
@@ -2,12 +2,55 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MineSweeperActionListener implements ActionListener {
-    public MineSweeperActionListener(MineSweeper mineSweeper,MineSweeperHelper mineSweeperHelper){
+    private MineSweeper mineSweeper;
+    private MineSweeperHelper mineSweeperHelper;
 
+    public MineSweeperActionListener(MineSweeper mineSweeper,MineSweeperHelper helper){
+        this.mineSweeper=mineSweeper;
+        mineSweeperHelper=helper;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Exit")){
+            System.exit(0);
+        }
+        else if(e.getActionCommand().equals("New Game")){
+            mineSweeperHelper.newGame(mineSweeper.rows,mineSweeper.columns);
+            return;
+        }
+        else if(e.getActionCommand().equals("Small (8*8, 10 Mines)")){
+            int previousRows = mineSweeper.rows;
+            int previousColumns = mineSweeper.columns;
 
+            mineSweeper.rows=8;
+            mineSweeper.columns=8;
+            mineSweeper.mines=10;
+
+            mineSweeperHelper.newGame(previousRows,previousColumns);
+            return;
+        }
+        else if(e.getActionCommand().equals("Medium (16*16,40 Mines)")){
+            int previousRows = mineSweeper.rows;
+            int previousColumns = mineSweeper.columns;
+
+            mineSweeper.rows=16;
+            mineSweeper.columns=16;
+            mineSweeper.mines=40;
+
+            mineSweeperHelper.newGame(previousRows,previousColumns);
+            return;
+        }
+        else if(e.getActionCommand().equals("Large (16*32, 100 Mines)")){
+            int previousRows = mineSweeper.rows;
+            int previousColumns = mineSweeper.columns;
+
+            mineSweeper.rows=16;
+            mineSweeper.columns=32;
+            mineSweeper.mines=100;
+
+            mineSweeperHelper.newGame(previousRows,previousColumns);
+            return;
+        }
     }
 }
--- src/MineSweeperMouseListener.java
+++ src/MineSweeperMouseListener.java
@@ -1,15 +1,50 @@
+import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 
 public class MineSweeperMouseListener implements MouseListener{
-    public MineSweeperMouseListener(MineSweeper mineSweeper,MineSweeperHelper mineSweeperHelper){
 
+    private MineSweeper mineSweeper;
+    private MineSweeperHelper mineSweeperHelper;
+
+    public MineSweeperMouseListener(MineSweeper mineSweeper,MineSweeperHelper helper){
+        this.mineSweeper=mineSweeper;
+        mineSweeperHelper=helper;
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        JButton clickButton = (JButton)e.getSource();
+        for (int i = 0; i < mineSweeper.rows; i++) {
+            for (int j = 0; j < mineSweeper.columns; j++) {
+                if(clickButton == mineSweeper.mineButtons[i][j]){
+                    MineField.gameState state;
+                    if(e.getButton() == MouseEvent.BUTTON1){
+                        state = mineSweeper.mineField.resolveClick(i,j,true);
+                        if(state == MineField.gameState.CONTINUE){
+                            if(mineSweeper.mineField.getMineFlag(i,j) == Mine.flagState.UNKNOWN){
+                                clickButton.removeMouseListener(this);
+                            }
+                        }
+                    }
+                    else{
+                        state = mineSweeper.mineField.resolveClick(i,j,false);
+                    }
+                    if(state == MineField.gameState.WIN){
+                        mineSweeperHelper.endGame(true);
+                    }
+                    else if(state == MineField.gameState.LOSE){
+                        mineSweeperHelper.endGame(false);
+                    }
+                    else{
+                        mineSweeperHelper.updateButtons();
+                    }
 
+                }
+            }
+            mineSweeperHelper.updateLabels();
+        }
     }
 
     @Override
