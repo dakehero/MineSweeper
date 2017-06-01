eb6bcc524771d2c13bc3bdd54ed4c55d0a3ed0bc
伍淼 <wumiao2008@outlook.com>
2017-06-01 23:39:23 +0800

    HighScore

--- src/HighScoreItem.java
+++ src/HighScoreItem.java
@@ -1,6 +1,12 @@
+/**
+ * HighScoreItem类的声明和包装
+ * <p>4个公开的变量：名字，时间秒，时间毫秒，枚举类型的难度。一个构造函数<br>
+ *  @author 伍淼
+ */
 
 public class HighScoreItem {
-    public HighScoreItem(String usrName, int seconds, int millis, Difficulty difficulty) {
+
+    public HighScoreItem() {
         this.usrName = usrName;
         this.seconds = seconds;
         this.millis = millis;
